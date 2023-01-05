9bdaba5dd24e4b043119213b40bd2c77023848f8
AmanVatsSharma <aman95026@gmail.com>
2023-01-05 18:09:16 +0530

    feat: Add basic CoreModule to organize shared stuff

---
+++ src/core/core.module.ts
@@ -0,0 +1,26 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { TenantGuard } from './guards/tenant.guard';
+import { RedisProvider } from './cache/redis.provider';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
+  providers: [
+    PrismaService,
+    TenantGuard,
+    RedisProvider
+  ],
+  exports: [
+    PrismaService,
+    TenantGuard,
+    RedisProvider
+  ],
+})
+export class CoreModule {} 
