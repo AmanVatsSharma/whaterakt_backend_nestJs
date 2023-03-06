0d7fc6cc38169138c9ba8e5ef46194c50db4984d
AmanVatsSharma <aman95026@gmail.com>
2023-03-06 12:27:53 +0530

    feat: Create ContactModule

---
+++ src/contact/contact.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ContactResolver } from './contact.resolver';
+import { ContactService } from './contact.service';
+import { PrismaService } from 'src/prisma.service';
+@Module({
+  providers: [ContactResolver, ContactService, PrismaService],
+  exports: [ContactService],
+})
+export class ContactModule {}
